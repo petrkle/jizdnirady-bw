75e0155f564c9857cdf33e87ad63f86c0bd879ba
Petr <petr@kle.cz>
2012-07-12 16:08:01 +0200

    Skrývání obrázků, odbarvení.

---
+++ jr.js
@@ -0,0 +1,4 @@
+$('img[src|="img/minister.gif"]').css('display','none');
+$('img[src|="img/logos.gif"]').css('display','none');
+$('img[src|="img/blind.gif"]').css('display','none');
+$('img[src|="http://www.w3.org/Icons/valid-html401"]').css('display','none');
